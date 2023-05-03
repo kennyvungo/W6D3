bbdf0705613ba5f81318d5aa037002eb586564e7
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-03 15:32:25 -0700

    finished phase 3

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
-        # @users = User.all
-        # render json: @users
+        @users = User.all
+        render json: @users
     end
 
-    # def create
-    #     @user = @users
-    # end
+    def create
+        render json: params
+    end
 
+    def show
+        render json: params
+    end
 
+    def users_params
+
+    end
 end
