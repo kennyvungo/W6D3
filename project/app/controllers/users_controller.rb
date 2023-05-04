1b65ee85e77c008a4276edad48d2d87d0f7e2d32
kennyvngo <kennyvungo@gmail.com>
2023-05-04 16:33:03 -0700

    finished search, starting bonus now

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
+
+        if params[:id]
+            @users = User.find(params[:id])
+        else 
+            @users = User.all
+        end
         render json: @users
     end
 
